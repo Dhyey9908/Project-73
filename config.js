9300315ced69487fdd89d316546e2990d9dfc479
Dhyey9908 <72203705+Dhyey9908@users.noreply.github.com>
2022-01-25 14:06:07 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -2,9 +2,14 @@ import firebase from 'firebase'
 require("@firebase/firestore")
 
 const firebaseConfig = {
- //Add configuration here
- 
+  apiKey: "AIzaSyB7-hbLpq1XjKJ9DLBge4nmU-aj-FAOqfo",
+  authDomain: "lets-ride-7b39c.firebaseapp.com",
+  projectId: "lets-ride-7b39c",
+  storageBucket: "lets-ride-7b39c.appspot.com",
+  messagingSenderId: "1042552926420",
+  appId: "1:1042552926420:web:ee1e1448c91ee04717cb41"
 };
+
   firebase.initializeApp(firebaseConfig);
 
   export default firebase.firestore()
